648ec7c6657d44652309a6915d7d8ac142283f41
softwaredevPK <pskotarba@gmail.com>
2021-05-07 21:10:45 +0200

    Project renamed to eMenu

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'E_restaurant.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eMenu.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
